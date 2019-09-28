3c681db57e167d7edfe42d622b14bd0aeb218c45
sang1365 <55378321+sang1365@users.noreply.github.com>
2019-09-28 23:00:42 +0900

    main수정본
    main layout 수정본이랑 채팅창 레이아웃입니다. 아직 기능구현은 하지 않은 상태이며 intent를 이용해 main과 chat 사이를 이동하는 것이 가능합니다.

---
+++ mainactivity/ChatActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mobile_pt
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.main_chat.*
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_chat)
+
+        BottomList_Diet.setOnClickListener{
+            val intent1= Intent(this, MainActivity::class.java)
+            startActivity(intent1)
+        }
+    }
+}
---
+++ mainactivity/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.mobile_pt
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.main_layout.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+
+        BottomList_Chat.setOnClickListener{
+            val intent1= Intent(this, ChatActivity::class.java)
+            startActivity(intent1)
+        }
+    }
+
+
+}
